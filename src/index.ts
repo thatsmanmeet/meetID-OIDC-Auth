e408aac7baa5b81df9bff06eefa9ad6f84d318fc
thatsmanmeet <120583077+thatsmanmeet@users.noreply.github.com>
2026-05-04 11:28:43 +0530

    update info

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ async function main() {
   try {
     const server = createServer(createExpressApplication());
     server.listen(env.PORT, () =>
-      console.log(`Server is running on  http://localhost:${env.PORT}`),
+      console.log(
+        `Server is running on  http://localhost:${env.PORT}, site: ${env.SERVER_URL}`,
+      ),
     );
   } catch (error) {
     console.log(`Something went wrong. ${error}`.red);
